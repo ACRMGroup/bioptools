44d27196705b03682c4bc22c388b9daea919e8d3
Andrew Martin <Andrew C.R. Martin>
2015-03-04 19:20:03 +0000

    pdbgetchain now supports referncing chains by number

--- src/pdbgetchain.c
+++ src/pdbgetchain.c
@@ -96,9 +96,6 @@
 /* Prototypes
 */
 int main(int argc, char **argv);
-void WritePDBChains(FILE *out, WHOLEPDB *wpdb, char *chains, 
-                    BOOL numeric);
-int  WritePDBChainsByNumber(FILE *out, PDB *pdb, char *chains);
 void Usage(void);
 char **ParseCmdLine(int argc, char **argv, char *infile, char *outfile,
                     BOOL *numeric, BOOL *atomsOnly);
@@ -160,7 +157,6 @@ int main(int argc, char **argv)
          {
             SelectPDBChains(wpdb, chains, numeric);
             WriteWholePDB(out, wpdb);
-/*            WritePDBChains(out, wpdb, chains, numeric); */
          }
       }
    }
@@ -206,13 +202,31 @@ void SelectPDBChains(WHOLEPDB *wpdb, char **chains, BOOL numeric)
 BOOL ValidChain(PDB *pdb, char **chains, BOOL numeric)
 {
    int i;
-/*   static int ChainCount = 0; */
-   
-   for(i=0; ((chains[i] != NULL) && (chains[i][0] != '\0')); i++)
+   static int sChainCount = 0;
+
+   if(numeric)
    {
-      if(CHAINMATCH(pdb->chain, chains[i]))
-         return(TRUE);
+      sChainCount++;
+      for(i=0; ((chains[i] != NULL) && (chains[i][0] != '\0')); i++)
+      {
+         int numericChain;
+         
+         if(sscanf(chains[i], "%d", &numericChain))
+         {
+            if(sChainCount == numericChain)
+               return(TRUE);
+         }
+      }
    }
+   else
+   {
+      for(i=0; ((chains[i] != NULL) && (chains[i][0] != '\0')); i++)
+      {
+         if(CHAINMATCH(pdb->chain, chains[i]))
+            return(TRUE);
+      }
+   }
+   
    return(FALSE);
 }
 
@@ -230,177 +244,6 @@ PDB *FindEndOfChain(PDB *chain)
       continue;
    }
    return(p);
-}
-
-
-/************************************************************************/
-/*>void WritePDBChains(FILE *out, WHOLEPDB *wpdb, char *chains, 
-                       BOOL numeric)
-   ------------------------------------------------------------
-*//**
-
-   \param[in]      *out      Output file pointer
-   \param[in]      *pdb      WHOLEPDB structure containing PDB linked 
-   \param[in]      *chains   Chain names to be written to output file
-   \param[in]      numeric   Chains are specified numerically
-
-   Like blWritePDB(), but takes a list of chain names to be written. Other
-   chains are not written to the output.
-
-   If chain `0' is specified and no chain 0 exists, then the empty chain
-   name is written
-
--  07.02.97 Original   By: ACRM
--  24.07.97 Modified to handle chain 0 as being the empty chain if
-            chain 0 doesn't exist
--  06.01.99 Added numeric parameter
--  22.05.09 Now takes WHOLEPDB rather than PDB with keepHeader
--  22.07.14 Renamed deprecated functions with bl prefix. By: CTP
--  13.02.15 Now always keeps header  By: ACRM
--  23.02.15 Modified to use blWriteTerCard() and new version of 
-            blWriteWholePDBTrailer()
-*/
-void WritePDBChains(FILE *out, WHOLEPDB *wpdb, char *chains, BOOL numeric)
-{
-   PDB *p;
-   char PrevChain;
-   BOOL FoundZero = FALSE,
-        Written   = FALSE;
-   PDB  *pdb = wpdb->pdb;
-   int  numTer = 0;
-   
-   blWriteWholePDBHeader(out, wpdb);
-
-   if(numeric)
-   {
-      numTer = WritePDBChainsByNumber(out, pdb, chains);
-   }
-   else
-   {
-      PDB *prev = NULL;
-      PrevChain = '\0';
-      
-      for(p=pdb; p!=NULL; NEXT(p))
-      {
-         if(strchr(chains,p->chain[0]))
-         {
-            if((PrevChain != p->chain[0]) && (PrevChain != '\0'))
-            {
-               /* Chain change, insert TER card                         */
-               blWriteTerCard(out, prev);
-               numTer++;
-            }
-            blWritePDBRecord(out,p);
-            PrevChain = p->chain[0];
-            Written = TRUE;
-         }
-         if(p->chain[0] == '0')
-            FoundZero = TRUE;
-
-         prev=p;
-      }
-      if(Written)
-      {
-         Written = FALSE;
-         blWriteTerCard(out, prev);
-         numTer++;
-      }
-      
-      /* If we were looking for a chain zero and there wasn't a chain 
-         with that name, then we write the chain with a blank chain name
-      */
-      if(strchr(chains,'0') && !FoundZero)
-      {
-         for(p=pdb; p!=NULL; NEXT(p))
-         {
-            if(p->chain[0] == ' ')
-            {
-               blWritePDBRecord(out,p);
-               Written = TRUE;
-            }
-         }
-         if(Written)
-         {
-            Written = FALSE;
-            blWriteTerCard(out, prev);
-            numTer++;
-         }
-      }
-   }
-   
-   blWriteWholePDBTrailer(out, wpdb, numTer);
-}
-
-
-/************************************************************************/
-/*>BOOL WritePDBChainsByNumber(FILE *out, PDB *pdb, char *chains)
-   --------------------------------------------------------------
-*//**
-
-   \param[in]      *out     Output file pointer
-   \param[in]      *pdb     PDB linked list
-   \param[in]      *chains  Chain numbers to be written to output file
-
-   Like blWritePDB(), but takes a list of chain numbers to be written. 1
-   is the first chain, 2 the second up to 0 being the 10th. Other
-   chains are not written to the output.
-
--  06.01.99 Original   By: ACRM
--  22.07.14 Renamed deprecated functions with bl prefix. By: CTP
--  23.02.15 Modified to use blWriteTerCard() and new version of 
-            blWriteWholePDBTrailer()
-            Now returns number of TER cards written
-*/
-int WritePDBChainsByNumber(FILE *out, PDB *pdb, char *chains)
-{
-   PDB  *p,
-        *prev=NULL;
-   char PrevChain,
-        ThisChainChar;
-   int  ThisChainNum = 1,
-        numTer       = 0;
-   BOOL Written      = FALSE,
-        Warned       = FALSE;
-   
-
-   PrevChain = pdb->chain[0];
-   ThisChainChar = ITOCHAR(ThisChainNum);
-   
-   for(p=pdb; p!=NULL; NEXT(p))
-   {
-      if(PrevChain != p->chain[0])
-      {
-         /* Chain change, insert TER card                               */
-         if(Written)
-         {
-            blWriteTerCard(out, prev);
-            numTer++;
-            Written = FALSE;
-         }
-         PrevChain = p->chain[0];
-         ThisChainNum++;
-         ThisChainChar = ITOCHAR(ThisChainNum);
-         if(!Warned && (ThisChainChar > '9'))
-         {
-            fprintf(stderr,"Warning: More than 10 chains in PDB file\n");
-            Warned = TRUE;
-         }
-      }
-      if(strchr(chains,ThisChainChar))
-      {
-         blWritePDBRecord(out,p);
-         Written = TRUE;
-      }
-      prev = p;
-   }
-
-   if(Written)
-   {
-      blWriteTerCard(out, prev);
-      numTer++;
-   }
-
-   return(numTer);
 }
 
 
@@ -427,22 +270,23 @@ void Usage(void)
    fprintf(stderr,"\npdbgetchain V2.0 (c) 1997-2015 Dr. Andrew C.R. \
 Martin, UCL\n");
 
-   fprintf(stderr,"\nUsage: pdbgetchain [-n] [-l] [-a] \
+   fprintf(stderr,"\nUsage: pdbgetchain [-n] [-a] \
 chain[,chain[...]] [in.pdb [out.pdb]]\n");
    fprintf(stderr,"       -n Specify chains numerically: 1 is the first \
-chain, 2 the second,\n");
-   fprintf(stderr,"          etc.\n");
+chain, 2 the\n");
+   fprintf(stderr,"          second, etc.\n");
    fprintf(stderr,"       -a ATOMs only (discard HETATMs)\n");
 
    fprintf(stderr,"\npdbgetchain reads a PDB file and write out only \
 those chains specified\n");
    fprintf(stderr,"on the command line. If input and output filenames \
 are not given\n");
-   fprintf(stderr,"I/O is through standard input/output\n");
-   fprintf(stderr,"The headers are kept (the -k option in previous \
-versions is now deprecated)\n");
-   fprintf(stderr,"but may contain references to chains that are no \
-longer present.\n\n");
+   fprintf(stderr,"I/O is through standard input/output.\n");
+   fprintf(stderr,"\nThe -k (keep headers) and -l (take lowercase chain \
+names) options in\n");
+   fprintf(stderr,"previous versions are now deprecated.\n");
+   fprintf(stderr,"\nHeaders may contain references to chains that are \
+no longer present.\n\n");
    fprintf(stderr,"If the program is called as getchain rather than \
 pdbgetchain, the old\n");
    fprintf(stderr,"behaviour of only accepting one-character chain names \
